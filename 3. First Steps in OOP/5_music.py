482507e82e541b6aa1913074623cfbf462f941a9
Plam <117172634+plamenmarinov@users.noreply.github.com>
2023-07-16 08:44:46 +0300

    Files Added

---
+++ 3. First Steps in OOP/5_music.py
@@ -0,0 +1,16 @@
+class Music:
+    def __init__(self, title: str, artist: str, lyrics: str):
+        self.title = title
+        self.artist = artist
+        self.lyrics = lyrics
+
+    def print_info(self) -> str:
+        return f'This is "{self.title}" from "{self.artist}"'
+
+    def play(self) -> str:
+        return f"{self.lyrics}"
+
+
+song = Music("Hello", "Adele", "Lyrics")
+print(song.print_info())
+print(song.play())
